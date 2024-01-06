806bbe748444fe1957b2d16c6996d7f2f8f6612c
Huy Pham <phamduchuy159@gmail.com>
2024-01-06 21:22:57 +0700

    New files

--- crawl_data/crawl_data.py
+++ crawl_data/crawl_data.py
@@ -10,15 +10,15 @@ from selenium.webdriver.support import expected_conditions as ec
 warnings.filterwarnings('ignore')
 OPTIONS = webdriver.ChromeOptions()
 OPTIONS.add_argument('--disable-notifications')
-# DRIVER = webdriver.Chrome(options=OPTIONS)
+DRIVER = webdriver.Chrome(options=OPTIONS)
 
 
 @hydra.main(config_path='../config', config_name='crawlparameters')
 def get_website(config):
   for key, item in config.news.items():
-    print(key, item)
-  # driver.get(config.news.thanhnien)
-  # driver.maximize_window()
+    # print(key, item)
+    DRIVER.get(config.news[key])
+    DRIVER.maximize_window()
 
 if __name__ == '__main__':  
   get_website()
