3478c2acb632d9e8290a6e901c39913b62631bc0
Huy Pham <phamduchuy159@gmail.com>
2024-01-06 22:55:24 +0700

    New files

--- crawl_data/general_methods.py
+++ crawl_data/general_methods.py
@@ -1,4 +1,5 @@
 import warnings
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,6 +22,16 @@ def find_elements_by_xpath(driver, xpath):
 
 def find_elements_by_css(driver, css_selector):
     return driver.find_elements(by=By.CSS_SELECTOR, value=css_selector)
+
+
+def scroll_down(driver, scroll_value):
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")
+    scroll_value += 500
+    if scroll_value > scroll_height:
+        scroll_value = scroll_height
+    driver.execute_script(f"window.scrollTo(0, {scroll_value});")
+    time.sleep(3)
+    return scroll_value
 
 
 def driver_wait_by_xpath(driver, xpath, seconds):
