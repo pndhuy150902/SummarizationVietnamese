5f50aa7fd9636953c25194c35787d20f59711bec
Huy Pham <phamduchuy159@gmail.com>
2024-01-06 21:59:28 +0700

    New files

---
+++ crawl_data/main_crawl.py
@@ -0,0 +1,32 @@
+import warnings
+import hydra
+import crawl_thanhnien_news
+import crawl_tuoitre_news
+import crawl_vnexpress_news
+from selenium import webdriver
+
+warnings.filterwarnings('ignore')
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.add_argument('--disable-notifications')
+DRIVER = webdriver.Chrome(options=OPTIONS)
+ACTIONS_MAP = {
+    'thanhnien': crawl_thanhnien_news.get_news(DRIVER),
+    'tuoitre': crawl_tuoitre_news.get_news(DRIVER),
+    'vnexpress': crawl_vnexpress_news.get_news(DRIVER)
+}
+
+
+def save_data(data):
+    pass
+
+
+@hydra.main(config_path='../config', config_name='crawlparameters')
+def click_website_and_get_data(config):
+    for key, item in config.news.items():
+        # print(key, item)
+        DRIVER.get(config.news[key])
+        DRIVER.maximize_window()
+
+
+if __name__ == '__main__':
+    click_website_and_get_data()
