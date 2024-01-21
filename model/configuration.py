9e80026aeb2e0eb5a399985ea6e89c7f324aa123
Huy Pham <phamduchuy159@gmail.com>
2024-01-22 00:06:58 +0700

    Update files

--- model/configuration.py
+++ model/configuration.py
@@ -112,6 +112,7 @@ def prepare_model(model_name):
         model_name,
         device_map='auto',
         trust_remote_code=True,
+        use_flash_attention_2=True,
         quantization_config=bnb_config
     )
     model.config.pad_token_id = tokenizer.pad_token_id
