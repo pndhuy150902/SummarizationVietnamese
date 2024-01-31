e5c1826035c75b697a6d731ac6b24577eb5d8644
Huy Pham <phamduchuy159@gmail.com>
2024-01-31 15:45:28 +0700

    Update files

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 import warnings
+import argparse
 import os
 warnings.filterwarnings('ignore')
 
-# Install required library for this project
-os.system('pip install -r requirements.txt')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conda', action='store_true')
+    parser.add_argument('--pip', action='store_false', dest='conda')
+    args = parser.parse_args()
+    if args.conda is False:
+        # Install required library for this project
+        os.system('pip install -r requirements.txt')
+    else:
+        os.system('conda install --file requirements.txt')
