34cc48b2f18adbbb1c8661ccace9b41eb9a48ea0
Huy Pham <phamduchuy159@gmail.com>
2024-02-20 21:40:52 +0700

    Update files

--- model/configuration.py
+++ model/configuration.py
@@ -10,8 +10,8 @@ warnings.filterwarnings('ignore')
 
 def prepare_lora_configuration():
     lora_config = LoraConfig(
-        r=64,
-        lora_alpha=16,
+        r=32,
+        lora_alpha=64,
         target_modules=[
             'q_proj',
             'k_proj',
