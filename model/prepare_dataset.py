071f74459cf76c78b23fda1c885f7cbdbea576be
Huy Pham <phamduchuy159@gmail.com>
2024-05-13 20:46:38 +0700

    Update new

--- model/prepare_dataset.py
+++ model/prepare_dataset.py
@@ -22,6 +22,34 @@ def prepare_prompt_for_title(i, df):
     return prompt
 
 
+def prepare_prompt_questions(i, df):
+    context = df.iloc[i]['context']
+    summarization = df.iloc[i]['summarization']
+    question = df.iloc[i]['questions']
+    answer = df.iloc[i]['answers']
+    prompt = f"""<s>[INST] Bạn là một trợ lí AI. Bạn được giao một nhiệm vụ là tóm tắt nội dung sau bằng tiếng Việt, dựa vào tất cả các thông tin dưới đây:
+Nội dung cần tóm tắt: {context}
+Câu hỏi: {question}
+Câu trả lời: {answer}
+Hãy đảm bảo rằng nội dung tóm tắt của bạn phải nắm bắt được thông tin quan trọng từ nội dung cần tóm tắt và bỏ qua những thông tin không liên quan. [/INST] {summarization}</s>"""
+    return prompt
+
+
+def prepare_prompt_for_title_questions(i, df):
+    title = df.iloc[i]['title']
+    context = df.iloc[i]['context']
+    summarization = df.iloc[i]['summarization']
+    question = df.iloc[i]['questions']
+    answer = df.iloc[i]['answers']
+    prompt = f"""<s>[INST] Bạn là một trợ lí AI. Bạn được giao một nhiệm vụ là tóm tắt nội dung sau bằng tiếng Việt, dựa vào tất cả các thông tin dưới đây:
+Nội dung cần tóm tắt: {context}
+Tiêu đề: {title}
+Câu hỏi: {question}
+Câu trả lời: {answer}
+Hãy đảm bảo rằng nội dung tóm tắt của bạn phải nắm bắt được thông tin quan trọng từ nội dung cần tóm tắt và bỏ qua những thông tin không liên quan. [/INST] {summarization}</s>"""
+    return prompt
+
+
 def read_dataset(config):
     train_data_no_title = pd.read_csv(config.processed_data.train_data)
     # valid_data = pd.read_csv(config.processed_data.valid_data)
@@ -32,9 +60,9 @@ def read_dataset(config):
 
 def prepare_dataset(config):
     train_data_no_title, train_data_with_title, test_data = read_dataset(config)
-    train_prompts_no_title = [prepare_prompt(i, train_data_no_title) for i in range(len(train_data_no_title))]
-    train_prompts_with_title = [prepare_prompt_for_title(i, train_data_with_title) for i in range(len(train_data_with_title))]
-    test_prompts = [prepare_prompt(i, test_data) for i in range(len(test_data))]
+    train_prompts_no_title = [prepare_prompt_questions(i, train_data_no_title) for i in range(len(train_data_no_title))]
+    train_prompts_with_title = [prepare_prompt_for_title_questions(i, train_data_with_title) for i in range(len(train_data_with_title))]
+    test_prompts = [prepare_prompt_questions(i, test_data) for i in range(len(test_data))]
     train_prompts = train_prompts_with_title + train_prompts_no_title
     train_prompts = sorted(train_prompts, key=len, reverse=False)
     dataset = DatasetDict({
