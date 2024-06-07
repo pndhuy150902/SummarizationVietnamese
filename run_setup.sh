9f4df549e7add901543728900e83ded535c863e4
pndhuy150902 <phamduchuy159@gmail.com>
2024-06-07 11:12:10 +0700

    Update config

--- run_setup.sh
+++ run_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get install libaio-dev mpich vim git-lfs
+apt-get install libaio-dev mpich vim git-lfs -y
 CMAKE_ARGS="-DLLAMA_CUDA=on" FORCE_CMAKE=1 pip install llama-cpp-python
 while getopts ':pc:h' opt; do
   case "$opt" in
